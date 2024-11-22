8a11a06f83ac2238ea9d7a91951bcefc6875dd6b
micha <ST10434179@vcconnect.edu.za>
2024-11-22 23:29:29 +0200

    ok for sure final commit

--- Michael_Herring_ST10434179_MAST_Part2-master/types.ts
+++ Michael_Herring_ST10434179_MAST_Part2-master/types.ts
@@ -1,22 +1,7 @@
-// types.ts
-/*export type RootStackParamList = {
-    Home: { newItem?: { dishName: string, description: string, course: string, price: number } };
-    AddItems: undefined;
-    Filter: { menuItems: { dishName: string; description: string; course: string; price: number }[] };
-  };
-  */
- /*
   export type RootStackParamList = {
-    Home: { newItem?: { dishName: string; description: string; course: string; price: number } }; // Allow newItem parameter
-    AddItems: undefined;
-    Filter: { menuItems: { dishName: string; description: string; course: string; price: number }[] };
-  };
-  */
-
-  export type RootStackParamList = {
-    Home: { menuItems: MenuItem[]; newItem?: MenuItem }; // Home screen expects menuItems
-    AddItems: { menuItems?: MenuItem[] }; // AddItems screen expects an optional menuItems
-    Filter: { menuItems: MenuItem[] }; // Filter screen expects menuItems
+    Home: { menuItems: MenuItem[]; newItem?: MenuItem }; 
+    AddItems: { menuItems?: MenuItem[] }; 
+    Filter: { menuItems: MenuItem[] }; 
   };
   
   export type MenuItem = {
